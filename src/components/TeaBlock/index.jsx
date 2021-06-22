f89b213debbb9176eeee0cd8322f5193c5cc007c
sa-toris <satoris88@gmail.com>
2021-06-22 22:18:09 +0700

    created header and content

--- src/components/TeaBlock/index.jsx
+++ src/components/TeaBlock/index.jsx
@@ -137,7 +137,7 @@ function TeaBlock({ id, name, imageUrl, price, weight, onClickAddTea, addedCount
                 active: activeWeight === index,
                 disabled: !weight.includes(scale),
               })}>
-              {scale} грамм
+              {scale} гр.
             </li>
           ))}
         </ul>
